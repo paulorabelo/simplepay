5147b3c16219ec3ad762cd43332f3031ec86b7aa
Paulo Rabelo <paulorabelo@hotmail.com>
2023-09-01 20:00:41 -0300

    feat: added RequestMap reconfig PostMap and GetMap

--- src/main/java/com/simplepay/controllers/UserController.java
+++ src/main/java/com/simplepay/controllers/UserController.java
@@ -1,15 +1,32 @@
 package com.simplepay.controllers;
 
 import com.simplepay.domain.user.User;
+import com.simplepay.dtos.UserDTO;
+import com.simplepay.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/users")
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+
 public class UserController {
 
-    @PostMapping
-    public ResponseEntity<User> createUser(UserDTO user){
+    @Autowired
+    private UserService userService;
 
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserDTO user){
+        User newUser = userService.createUser(user);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = this.userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 }
