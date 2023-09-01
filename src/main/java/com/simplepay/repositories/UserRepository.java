3ff8a2f1e0c5d3908686e1b0ad2efa2f443dd3cd
Paulo Rabelo <paulorabelo@hotmail.com>
2023-08-31 22:45:24 -0300

    feat: create UserRepository

---
+++ src/main/java/com/simplepay/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.simplepay.repositories;
+
+import com.simplepay.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserById(Long id);
+}
