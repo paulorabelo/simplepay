993acd7e84419d96d33d967947ab5ad15374b4ff
Paulo Rabelo <paulorabelo@hotmail.com>
2023-08-31 22:43:45 -0300

    feat: first commit - inicial config

---
+++ src/test/java/com/simplepay/SimplepayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplepay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplepayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
