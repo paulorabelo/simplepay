fc497f2155beac7800614f85fe10b5d813195ca6
Paulo Rabelo <paulorabelo@hotmail.com>
2023-09-02 17:29:12 -0300

    feat: config ControllerExceptionHandler

--- src/main/java/com/simplepay/infra/ControllerExceptionHandler.java
+++ src/main/java/com/simplepay/infra/ControllerExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.simplepay.infra;
 
+import com.simplepay.dtos.ExceptionDTO;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -8,4 +11,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity threatDuplicatedEntry(DataIntegrityViolationException exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Usuário já cadastrado", "400");
+        return ResponseEntity.badRequest().body(exceptionDTO);
+    }
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(EntityNotFoundException exception){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity threatGeneralExceptions(Exception exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "500");
+        return ResponseEntity.internalServerError().body(exceptionDTO);
+    }
 }
