63952a088f086094d37042f91236b1c67807866c
Paulo Rabelo <paulorabelo@hotmail.com>
2023-09-02 13:07:23 -0300

    feat: config TransactionController

--- src/main/java/com/simplepay/controllers/TransactionController.java
+++ src/main/java/com/simplepay/controllers/TransactionController.java
@@ -1,4 +1,25 @@
 package com.simplepay.controllers;
 
+import com.simplepay.domain.transaction.Transaction;
+import com.simplepay.dtos.TransactionDTO;
+import com.simplepay.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transactions")
 public class TransactionController {
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransacion(@RequestBody TransactionDTO transaction) throws Exception {
+        Transaction newTransaction = this.transactionService.createTransaction(transaction);
+        return new ResponseEntity<>(newTransaction, HttpStatus.OK);
+    }
 }
